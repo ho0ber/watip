010b2fe72719cba833918581085fe58e47608064
Samuel Colburn <ho0ber@gmail.com>
2019-04-18 11:21:11 -0400

    add header host

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require("http");
 http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "application/json"});
     let ip = request.headers["x-forwarded-for"] || request.connection.remoteAddress.replace(/^.*:/, "");
-    response.end(JSON.stringify({"ip": ip, "hostname": ""}, null, 4));
+    response.end(JSON.stringify({"ip": ip, "header_host": request.headers.host}, null, 4));
 }).listen(8080);
 
 console.log("wat");
